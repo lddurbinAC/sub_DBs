fd26cc2dd96e53af761fe1058a49bf94e64f8444
lddurbin <l.d.durbin@gmail.com>
2021-04-08 16:00:07 +1200

    Use lubridate to convert Standards date-as-character into date-as-date.

--- scripts/StandardsNZ.R
+++ scripts/StandardsNZ.R
@@ -1,18 +1,11 @@
-# Verify assumptions about the data using assertr
-check_file_structure <- . %>%
-  chain_start %>%
-  verify(ncol(.) == 4) %>% 
-  verify(has_all_names("Product", "User", "Date", "Time")) %>%
-  chain_end
-
 StandardsNZ_read_file <- function(x) {
   read.csv(x, stringsAsFactors = F, header=T)
 }
 
 StandardsNZ_Prep <- function(x) {
   x %>% distinct() %>%
-    clean_names() %>% 
-    mutate(reporting_period = format(as.Date(date, format="%d-%m-%Y"), "%Y-%m-01")) %>%
+    select(date = 3) %>% 
+    mutate(reporting_period = dmy(date)) %>% 
     group_by(reporting_period) %>%
     mutate(id = n_distinct(reporting_period), value = sum(id), metric_name = "views", database = "Standards Online New Zealand", publisher = "Standards New Zealand") %>% 
     distinct(reporting_period, metric_name, value = as.double(value), database, publisher) %>% 
@@ -25,5 +18,4 @@ directory_path = "data/raw/StandardsNZ"
 StandardsNZ <- lapply(paste(directory_path, list.files(directory_path), sep="/"), StandardsNZ_read_file) %>%
   bind_rows() %>% 
   distinct() %>% 
-  check_file_structure %>% 
   StandardsNZ_Prep
