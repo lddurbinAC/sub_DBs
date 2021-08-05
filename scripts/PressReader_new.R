2daa692f524ffdf038a78001cb49e457a73b2f45
lddurbin <l.d.durbin@gmail.com>
2021-08-05 16:04:31 +1200

    Clean up script

--- scripts/PressReader_new.R
+++ scripts/PressReader_new.R
@@ -13,26 +13,3 @@ PressReader_new <- lapply(files, PressReader_prep) %>%
   summarise(article_opens = sum(article_opens), sessions = sum(sessions), .groups = "drop") %>% 
   pivot_longer(c(article_opens, sessions), names_to = "metric_name") %>% 
   mutate(database = "PressReader", publisher = "PressReader")
-
-
-# comparison_data <- PressReader_new %>% 
-#   mutate(database = "PressReader_new") %>% 
-#   bind_rows(PressReader) %>% 
-#   mutate(metric_name = case_when(
-#     metric_name == "article_opens" ~ "views",
-#     !is.na(metric_name) ~ str_to_lower(metric_name)
-#   )) %>% 
-#   filter(reporting_period > date("2019-06-30")) 
-# 
-# comparison_data %>% 
-#   ggplot(aes(x = reporting_period, y = value, fill = database)) +
-#   geom_col(position = "dodge") +
-#   facet_grid(rows = vars(metric_name), scales = "free_y")
-# 
-# comparison_data %>%
-#   pivot_wider(names_from = database, values_from = value) %>% 
-#   mutate(change = round(((PressReader_new - PressReader)/PressReader)*100), 2) %>% 
-#   ggplot(aes(x = reporting_period, y = change)) +
-#   geom_col() +
-#   facet_grid(rows = vars(metric_name), scales = "free_y") +
-#   geom_text(aes(label = change), vjust = -0.5)
