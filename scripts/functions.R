952fe7d1bdf1cef46eda2533134e3597d3ff0dd1
lddurbin <l.d.durbin@gmail.com>
2021-12-07 15:08:29 +1300

    Read Digitales files from Excel rather than CSV

--- scripts/functions.R
+++ scripts/functions.R
@@ -14,17 +14,9 @@ clean_and_gather <- function(df, values, keyName) {
 
 # Verify and prepare data downloaded from the Digitales dashboard
 digitales_dashboard <- function(x) {
-  # Verify assumptions about the data using assertr
-  check_file_structure <- . %>%
-    chain_start %>%
-    verify(ncol(.) == 6) %>% 
-    verify(has_all_names("Customer", "Resource", "Year", "Month", "Click", "Renewal Date")) %>%
-    verify(ncol(select_if(., is.numeric)) == 1) %>% 
-    chain_end
-  
   # Take the raw data and transform it into the standardised output
-  df <- read_csv(x, col_types = "ccccnc") %>% 
-    check_file_structure
+  df <- read_excel(x, col_types = "ccccnc")
+  
   df %>% mutate(reporting_period = as.Date(paste("01", Month, Year, sep="/"), "%d/%B/%Y"), metric_name = "Views", publisher = "Digitales") %>% 
     select(everything(), -c(1, 3:4, 6), value = Click, database = Resource)
 }
